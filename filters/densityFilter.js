257ca72afd3746ea3c177e0f68f385fc8ee9f144
Antoine Paulet <antoinepaulet@orange.fr>
2025-02-24 14:38:57 +0100

    Update densityFilter.js

--- filters/densityFilter.js
+++ filters/densityFilter.js
@@ -592,9 +592,9 @@ function segmentOceanCandidate(cells) {
   });
   const C_avg = cells.reduce((sum, cell) => sum + cell.connectivity, 0) / cells.length;
   
-  // Mark cells as thin if connectivity < 0.3 * C_avg.
+  // Mark cells as thin if connectivity < 0.1 * C_avg.
   cells.forEach(cell => {
-    cell.thin = (cell.connectivity / C_avg) < 0.3;
+    cell.thin = (cell.connectivity / C_avg) < 0.1;
   });
   
   // Group thin cells via 26-neighbor flood-fill.
