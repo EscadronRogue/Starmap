31346fa93d73333ef4938afd20ee431a4300cce8
Antoine Paulet <antoinepaulet@orange.fr>
2025-03-03 17:36:49 +0100

    Update densityFilter.js

--- filters/densityFilter.js
+++ filters/densityFilter.js
@@ -1,16 +1,17 @@
-// filters/densityFilter.js
+// /filters/densityFilter.js
 
 import { DensityGridOverlay } from './densityGridOverlay.js';
 
 /**
  * Initializes the density overlay grid.
- * @param {number} maxDistance - Maximum distance for grid cells.
+ * @param {number} minDistance - Minimum distance to consider for density mapping.
+ * @param {number} maxDistance - Maximum distance to consider for density mapping.
  * @param {Array} starArray - Array of star objects.
  * @param {string} mode - "low" or "high" (default "low").
  * @returns {DensityGridOverlay} - The initialized grid overlay.
  */
-export function initDensityOverlay(maxDistance, starArray, mode = "low") {
-  const grid = new DensityGridOverlay(maxDistance, 2, mode);
+export function initDensityOverlay(minDistance, maxDistance, starArray, mode = "low") {
+  const grid = new DensityGridOverlay(minDistance, maxDistance, 2, mode);
   grid.createGrid(starArray);
   return grid;
 }
