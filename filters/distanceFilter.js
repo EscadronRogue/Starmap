f9e305976e587d448dafcdd968590b6badf555ca
Antoine Paulet <antoinepaulet@orange.fr>
2025-03-03 17:17:53 +0100

    Create distanceFilter.js

---
+++ filters/distanceFilter.js
@@ -0,0 +1,23 @@
+// /filters/distanceFilter.js
+
+/**
+ * Filters stars based on their Distance_from_the_Sun.
+ * Only stars with Distance_from_the_Sun between minDistance and maxDistance (in light years) are kept.
+ *
+ * @param {Array} stars - Array of star objects.
+ * @param {Object} filters - The filters object that should include minDistance and maxDistance.
+ * @returns {Array} - Filtered array of stars.
+ */
+export function applyDistanceFilter(stars, filters) {
+  // Default values: show stars from 0 to 20 LY.
+  const minDist = filters.minDistance !== null && filters.minDistance !== undefined
+    ? parseFloat(filters.minDistance)
+    : 0;
+  const maxDist = filters.maxDistance !== null && filters.maxDistance !== undefined
+    ? parseFloat(filters.maxDistance)
+    : 20;
+  return stars.filter(star => {
+    if (star.Distance_from_the_Sun === undefined) return false;
+    return star.Distance_from_the_Sun >= minDist && star.Distance_from_the_Sun <= maxDist;
+  });
+}
