6e149adfa6080c8391f0eeff119c4b2179fedf68
Antoine Paulet <antoinepaulet@orange.fr>
2025-03-06 18:18:15 +0100

    Update densityFilter.js

--- filters/densityFilter.js
+++ filters/densityFilter.js
@@ -8,10 +8,12 @@ import { DensityGridOverlay } from './densityGridOverlay.js';
  * @param {number} maxDistance - Maximum distance to consider for density mapping.
  * @param {Array} starArray - Array of star objects.
  * @param {string} mode - "low" or "high" (default "low").
+ * @param {number} [gridSize=2] - The size of each grid cell. (New slider-based value)
  * @returns {DensityGridOverlay} - The initialized grid overlay.
  */
-export function initDensityOverlay(minDistance, maxDistance, starArray, mode = "low") {
-  const grid = new DensityGridOverlay(minDistance, maxDistance, 2, mode);
+export function initDensityOverlay(minDistance, maxDistance, starArray, mode = "low", gridSize = 2) {
+  // We now pass gridSize into the constructor instead of always using 2
+  const grid = new DensityGridOverlay(minDistance, maxDistance, gridSize, mode);
   grid.createGrid(starArray);
   return grid;
 }
