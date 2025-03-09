e51ea11d57115cea6716ae88bc3201de15b67fb3
Antoine Paulet <antoinepaulet@orange.fr>
2025-03-09 22:08:16 +0100

    Create ConvexGeometry.js

---
+++ filters/ConvexGeometry.js
@@ -0,0 +1,46 @@
+// /filters/ConvexGeometry.js
+
+/**
+ * ConvexGeometry is a geometry representing the convex hull of a set of points.
+ * It depends on ConvexHull.js (provided in the same folder).
+ */
+
+import { BufferGeometry, Float32BufferAttribute } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.min.js';
+import { ConvexHull } from './ConvexHull.js';
+
+class ConvexGeometry extends BufferGeometry {
+
+	constructor( points ) {
+
+		super();
+
+		if ( points === undefined ) return;
+
+		// Build an array of vertices from the convex hull.
+		const vertices = [];
+
+		const convexHull = new ConvexHull().setFromPoints( points );
+
+		const faces = convexHull.faces;
+
+		for ( let i = 0; i < faces.length; i ++ ) {
+
+			const face = faces[ i ];
+			let edge = face.edge;
+			do {
+				const point = edge.head();
+				vertices.push( point.x, point.y, point.z );
+				edge = edge.next;
+			} while ( edge !== face.edge );
+
+		}
+
+		// Build geometry.
+		this.setAttribute( 'position', new Float32BufferAttribute( vertices, 3 ) );
+		this.computeVertexNormals();
+
+	}
+
+}
+
+export { ConvexGeometry };
